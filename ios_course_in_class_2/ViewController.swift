a715999232b6776fdfcd533b947646320acc1d3b
Chuxi Wang <wangchuxi94@gmail.com>
2021-02-13 12:14:11 -0800

    Initial Commit

---
+++ ios_course_in_class_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios_course_in_class_2
+//
+//  Created by Mialsy on 2/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
